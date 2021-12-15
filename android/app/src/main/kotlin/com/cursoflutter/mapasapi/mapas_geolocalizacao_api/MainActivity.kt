80902ef0a925b96057c8ca69d0441eb77f216715
Vitor Hugo Pereira Silva <vitor_hugo_ps@hotmail.com>
2021-12-15 10:01:59 -0300

    Projeto criado configuração inicial feita

---
+++ android/app/src/main/kotlin/com/cursoflutter/mapasapi/mapas_geolocalizacao_api/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.cursoflutter.mapasapi.mapas_geolocalizacao_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:mapas_geolocalizacao_api/Home.dart';
+
+void main() => runApp(const MaterialApp(
+  title: "Mapas e Geolocalização",
+  home: Home()
+));
