5695b0f324ae4a8491a84a8c1279aaf063fde7e1
Vitor Hugo Pereira Silva <vitor_hugo_ps@hotmail.com>
2021-12-23 13:45:28 -0300

    Build sagrada que conserta erros no build.gradle
    - Alterações para impedir erros no build,gradle feitas nos seguintes arquivos: build.gradle (app); build.gradle (android); gradle-wrapper.properties.
    - Quando alterações forem feitas a esses arquivos, rodar flutter clean antes de buildar novamente.

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,17 +1,85 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
+  Completer<GoogleMapController> _controllerCompleter = Completer();
+  Set<Marker> _marcadores = {};
+
+  _onMapCreated(GoogleMapController googleMapController) {
+    _controllerCompleter.complete(googleMapController);
+  }
+
+  _movimentarCamera() async {
+    GoogleMapController googleMapController = await _controllerCompleter.future;
+    googleMapController.animateCamera(CameraUpdate.newCameraPosition(
+        CameraPosition(target: LatLng(-21.5572576, -45.7295776), zoom: 20, tilt: 45, bearing: 85,)));
+  }
+
+  _carregarMarcadores(){
+    Set<Marker> _marcadorLocal = {};
+
+    Marker marcadorSJ = Marker(
+      markerId: MarkerId("marcador-saojoao"),
+      position: LatLng(-21.557267578362513, -45.72978815339992),
+      infoWindow: InfoWindow(title: "São João Supermercados")
+    );
+
+    Marker marcadorPdQ = Marker(
+      markerId: MarkerId("marcador-casapaoqueijo"),
+      position: LatLng(-21.548644742295426, -45.735878485787886),
+      infoWindow: InfoWindow(title: "Casa do Pão de Queijo")
+    );
+
+    _marcadorLocal.add(marcadorSJ);
+    _marcadorLocal.add(marcadorPdQ);
+
+    setState(() {
+      _marcadores = _marcadorLocal;
+    });
+  }
+
+  _recuperarLocalAtual() async {
+    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+
+    print("Localização atual: " + position.toString());
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // _carregarMarcadores();
+    _recuperarLocalAtual();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Mapas e Geolocalização"),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButton: FloatingActionButton(
+          onPressed: _movimentarCamera, child: Icon(Icons.done)),
+      body: Container(
+        child: GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition:
+              CameraPosition(target: LatLng(-21.548360, -45.747945), zoom: 16),
+          onMapCreated: _onMapCreated,
+          markers: _marcadores,
+        ),
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mapas_geolocalizacao_api/Home.dart';
 
 void main() => runApp(const MaterialApp(
+  debugShowCheckedModeBanner: false,
   title: "Mapas e Geolocalização",
   home: Home()
 ));
