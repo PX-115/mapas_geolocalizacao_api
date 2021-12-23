c0f0a7e18c2b15426ba3db1983da6740f6938cdb
Vitor Hugo Pereira Silva <vitor_hugo_ps@hotmail.com>
2021-12-23 14:47:19 -0300

    Adicionado listener para recuperar o local atual do usuário, mesmo que ele se locomova

--- lib/Home.dart
+++ lib/Home.dart
@@ -14,6 +14,8 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   Completer<GoogleMapController> _controllerCompleter = Completer();
+  CameraPosition _posicaoCamera =
+      CameraPosition(target: LatLng(-21.548360, -45.747945), zoom: 17);
   Set<Marker> _marcadores = {};
 
   _onMapCreated(GoogleMapController googleMapController) {
@@ -22,25 +24,29 @@ class _HomeState extends State<Home> {
 
   _movimentarCamera() async {
     GoogleMapController googleMapController = await _controllerCompleter.future;
-    googleMapController.animateCamera(CameraUpdate.newCameraPosition(
-        CameraPosition(target: LatLng(-21.5572576, -45.7295776), zoom: 20, tilt: 45, bearing: 85,)));
+    googleMapController
+        .animateCamera(CameraUpdate.newCameraPosition(_posicaoCamera));
   }
 
-  _carregarMarcadores(){
+  _carregarMarcadores() {
     Set<Marker> _marcadorLocal = {};
 
+    /* Marker marcadorPosicaoAtual = Marker(
+      markerId: MarkerId("marcador-posicao-atual"),
+      position: LatLng(-21.5484403, -45.7479973)
+    ); */
+
     Marker marcadorSJ = Marker(
-      markerId: MarkerId("marcador-saojoao"),
-      position: LatLng(-21.557267578362513, -45.72978815339992),
-      infoWindow: InfoWindow(title: "São João Supermercados")
-    );
+        markerId: MarkerId("marcador-saojoao"),
+        position: LatLng(-21.557267578362513, -45.72978815339992),
+        infoWindow: InfoWindow(title: "São João Supermercados"));
 
     Marker marcadorPdQ = Marker(
-      markerId: MarkerId("marcador-casapaoqueijo"),
-      position: LatLng(-21.548644742295426, -45.735878485787886),
-      infoWindow: InfoWindow(title: "Casa do Pão de Queijo")
-    );
+        markerId: MarkerId("marcador-casapaoqueijo"),
+        position: LatLng(-21.548644742295426, -45.735878485787886),
+        infoWindow: InfoWindow(title: "Casa do Pão de Queijo"));
 
+    // _marcadorLocal.add(marcadorPosicaoAtual);
     _marcadorLocal.add(marcadorSJ);
     _marcadorLocal.add(marcadorPdQ);
 
@@ -50,16 +56,36 @@ class _HomeState extends State<Home> {
   }
 
   _recuperarLocalAtual() async {
-    Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.best);
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    // print("Localização atual: " + position.toString());
 
-    print("Localização atual: " + position.toString());
+    setState(() {
+      _posicaoCamera = CameraPosition(
+          target: LatLng(position.latitude, position.longitude), zoom: 17);
+    });
+    _movimentarCamera();
+  }
+
+  _listenerLocalizacao() {
+    var geolocator = Geolocator();
+    var locationSettings =
+        LocationSettings(accuracy: LocationAccuracy.high, distanceFilter: 2);
+    Geolocator.getPositionStream(locationSettings: locationSettings)
+        .listen((Position position) {
+      setState(() {
+        _posicaoCamera = CameraPosition(
+            target: LatLng(position.latitude, position.longitude), zoom: 17);
+      });
+      _movimentarCamera();
+    });
   }
 
   @override
   void initState() {
     super.initState();
     // _carregarMarcadores();
-    _recuperarLocalAtual();
+    // _recuperarLocalAtual();
   }
 
   @override
@@ -68,16 +94,17 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: Text("Mapas e Geolocalização"),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      /* floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
       floatingActionButton: FloatingActionButton(
-          onPressed: _movimentarCamera, child: Icon(Icons.done)),
+          onPressed: _movimentarCamera, child: Icon(Icons.done)), */
       body: Container(
         child: GoogleMap(
           mapType: MapType.normal,
-          initialCameraPosition:
-              CameraPosition(target: LatLng(-21.548360, -45.747945), zoom: 16),
+          initialCameraPosition: _posicaoCamera,
           onMapCreated: _onMapCreated,
-          markers: _marcadores,
+          myLocationEnabled: true,
+          myLocationButtonEnabled: true,
+          // markers: _marcadores,
         ),
       ),
     );
